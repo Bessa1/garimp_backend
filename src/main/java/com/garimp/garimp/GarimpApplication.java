418a8fc14be2bf38c5daebb2b8cdf6bcb3f11731
Carlos Bessa <48933250+Bessa1@users.noreply.github.com>
2022-06-15 21:21:12 -0300

    Template inicial

---
+++ src/main/java/com/garimp/garimp/GarimpApplication.java
@@ -0,0 +1,14 @@
+package com.garimp.garimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarimpApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/garimp/garimp/GarimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garimp.garimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
